6e92fb0bd3ccefc98ef2c254a0864bab950c1867
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-09-28 17:35:46 +0300

    Выделен класс для управления сессией

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,13 +12,12 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(name="Alex", m_name="John", l_name="Johnson", n_name="s_Jhon", title="sometitle",
                         company="compnyname", address="someaddress", home_phone="4213123", mobile_phone="1212",
                         work_phone="somework", fax_number="faxnumber", email_1="asd", email_2="asdasd",
                         email_3="asdasda", homepage="asdasdc", second_address="asdasdasdcxzz",
                         secondary_phone="fwklmlsm", notes="skamdlkasmdlakmd"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
