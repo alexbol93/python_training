bac236cf1abd29eb7f6fdac69d458e4630ee682d
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-10-07 23:02:03 +0300

    Реализована проверка состояния сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="test", header="test_text", footer="test_text"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
