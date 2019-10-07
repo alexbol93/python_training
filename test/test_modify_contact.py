aee406f24097c21dabbad3d639a0aaec649ae6f3
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-10-07 22:10:03 +0300

    Реализована валидация текстуры

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,7 +3,6 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(name="Alex_m", m_name="John_m", l_name="Johnson_m", n_name="s_Jhon_m",
                                              title="sometitle_m", company="compnyname_m", address="someaddress_m",
                                              home_phone="4213123_m", mobile_phone="1212_m", work_phone="somework_m",
@@ -12,4 +11,3 @@ def test_modify_contact(app):
                                              second_address="_masdasdasdcxzz_m", secondary_phone="fwklmlsm_m",
                                              notes="skamdlkasmdlakmd_m"))
     app.return_to_home_page()
-    app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,12 +2,8 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify(Group(name="test_text_modify"))
-    app.session.logout()
 
 
 def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify(Group(header="test_text_modify"))
-    app.session.logout()
