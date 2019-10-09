f9fdb157c1c13345235ab9ba36b4bbf950307b72
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-10-09 22:58:57 +0300

    Исправление по замечаниям к 10 заданию

--- fixture/application.py
+++ fixture/application.py
@@ -22,7 +22,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url.endswith("localhost/addressbook")
+        if not (wd.current_url.endswith("/addressbook")
                 and (len(wd.find_elements_by_link_text("Last name")))) > 0:
             wd.get("http://localhost/addressbook/")
 
