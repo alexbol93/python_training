4d3ec22b547c1b9b60662bf1a5bd1fc3c845ef63
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-10-17 01:32:50 +0300

    Добавлены проверки количества групп в тесты создания, удаления и модификации групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,12 +2,18 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test_group"))
     app.group.modify(Group(name="test_text_modify"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test_group"))
     app.group.modify(Group(header="test_text_modify"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
