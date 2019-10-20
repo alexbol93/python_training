9c97c20fcf408b92f647b37f7b2085d9a284209d
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-10-20 15:04:43 +0300

    Добавлено сравнение списков груп после добавления и модификации

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,11 +3,15 @@ from model.group import Group
 
 def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
+    group = Group(name="test_text_modify")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name="test_group"))
-    app.group.modify(Group(name="test_text_modify"))
+    app.group.modify(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
