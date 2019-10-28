14a855b8b593b7f7dd46647724354cd935bde21e
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-10-29 00:44:42 +0300

    Добавлен генератор случайных тестовых данных для добавления контактов (генератор строки и номера)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_number(minlen, maxlen):
+    symbols = string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(minlen, maxlen))])
+
+
+testdata = [Contact(name=random_string("name", 10), m_name=random_string("m_name", 10),
+                    l_name=random_string("l_name", 10), n_name=random_string("n_name", 10),
+                    title=random_string("title", 10), company=random_string("c_name", 10),
+                    address=random_string("adress", 10), home_phone=random_number(4, 9), mobile_phone=random_number(4, 9),
+                    work_phone=random_number(4, 9), fax_number=random_number(4, 9), email_1=random_string("email@", 10),
+                    email_2=random_string("email@", 10), email_3=random_string("email@", 10),
+                    homepage=random_string("HP", 10), second_address=random_string("SA", 10),
+                    secondary_phone=random_number(4, 9), notes=random_string("notice", 10)) for i in range(5)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(name="Alex", m_name="John", l_name="Johnson", n_name="s_Jhon", title="sometitle",
-                               company="compnyname", address="someaddress", home_phone="4213123", mobile_phone="1212",
-                               work_phone="somework", fax_number="faxnumber", email_1="asd", email_2="asdasd",
-                               email_3="asdasda", homepage="asdasdc", second_address="asdasdasdcxzz",
-                               secondary_phone="fwklmlsm", notes="skamdlkasmdlakmd")
-
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
