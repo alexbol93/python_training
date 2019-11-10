5b660ac43f1f85b54365c92ba3bfecef6a1da159
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-11-10 15:55:00 +0300

    добавлен генератор тестовых данных для групп в формате json

--- data/group.py
+++ data/group.py
@@ -7,11 +7,4 @@ constant = [
     Group(name="name2", header="header2", footer="footer2")
 ]
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [Group(name=random_string("name", 10),
-             header=random_string("header", 10), footer=random_string("footer", 10)) for i in range(5)]
 
