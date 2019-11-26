56a2e7e9e33c5af69394d44a22e73bb07750ebc5
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2019-11-26 20:36:01 +0300

    Задание 22 Исправлен алгоритм поиска группы с контактом в предусловиях

--- test/delete_contact_from_group.py
+++ test/delete_contact_from_group.py
@@ -9,17 +9,26 @@ def test_del_contact_from_group(app, orm):
     if app.group.count() == 0:
         app.group.create(Group(name="Test", header="", footer=""))
     groups = orm.get_group_list()
-    group = random.choice(groups)    # Выберем случайную группу
-    users_in_group = orm.get_contacts_in_group(group)     # Получаем список пользователей в случайной группе
 
-    if len(users_in_group) == 0:    # Если пользователей нет, добавляем нового
-        all_users = orm.get_contact_list()
-        user = random.choice(all_users)
-        app.contact.add_contact_in_group(user.id, group.id)
-    users_in_group = orm.get_contacts_in_group(group)
-    user_to_del = random.choice(users_in_group)
-    app.contact.del_user_from_group(user_to_del.id, group.id)
+    contact = None
+    # Цикл по списку групп, который найдет первую непустую группу
+    for group in groups:
+        contacts = orm.get_contacts_in_group(group)
+        if len(contacts) == 0:
+            continue
+        else:
+            group_with_contact = group
+            contact = contacts[0]
+            break
+
+    # Если цикл прошел и все группы пустые то берем случайный контакт и помещаем его в случайную группу
+    if contact is None:
+        group_with_contact = random.choice(orm.get_group_list())
+        contact = random.choice(orm.get_contact_list())
+        app.contact.add_contact_in_group(contact.id, group_with_contact.id)
+
+    app.contact.del_user_from_group(contact.id, group_with_contact.id)
     new_users = orm.get_contacts_in_group(group)
-    assert user_to_del not in new_users  # Проверяем что пользователь удален из группы
+    assert contact not in new_users  # Проверяем что пользователь удален из группы
 
 
